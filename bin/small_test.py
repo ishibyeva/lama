2d901d5bcda356d76fad6fa42db7070985a5712a
ishibyeva <saf51523@gmail.com>
2022-05-17 20:32:33 +0300

    fix index in test

--- bin/small_test.py
+++ bin/small_test.py
@@ -22,13 +22,13 @@ def test_inpainting_with_standart_set_with_masks():
     #start_predict = ['python3', 'bin/predict.py', 'model.path=' + model_dir,
     #                'indir=' + testing_images, 'outdir=' + output_dir]
     
-    start_predict = ['python3', 'bin/predict.py', 'model.path=' + sys.argv[1],
-                    'indir=' + sys.argv[2], 'outdir=' + os.path.join(parent_dir, 'output')]
+    start_predict = ['python3', 'bin/predict.py', 'model.path=' + sys.argv[2],
+                    'indir=' + sys.argv[3], 'outdir=' + os.path.join(parent_dir, 'output')]
+
     start_time = time.time()
     proc = subprocess.Popen(start_predict, stdin = subprocess.PIPE, stderr = subprocess.PIPE)
     end_time = time.time()
-    output, error = proc.communicate()
-    print(error)
+    
     assert end_time - start_time < 1500.0, 'Failed: inpainting timeout'
 
 def test_check_directory_exists():
